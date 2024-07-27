f7070b69f256f109acfab808ae849e45c3d43ae5
Muhammad waqas kaleem <61083411+waqaskaleem007@users.noreply.github.com>
2024-07-27 20:23:28 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "www.w3.org"],
   },
+  output: 'export',
+  distDir: 'out', // This specifies the output directory
+  images: {
+    unoptimized: true, // Optional: if you encounter issues with images in static export
+  },
 };
 
 module.exports = nextConfig;
